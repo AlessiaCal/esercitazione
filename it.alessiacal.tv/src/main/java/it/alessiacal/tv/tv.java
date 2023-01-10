0c913c23af030ff62763d42f286543ccb5d9cacc
Alessia Calderone <alessia.calderone@emea.nttdata.com>
2023-01-10 10:26:20 +0100

    new

--- it.alessiacal.tv/src/main/java/it/alessiacal/tv/tv.java
+++ it.alessiacal.tv/src/main/java/it/alessiacal/tv/tv.java
@@ -1,7 +1,7 @@
 /* in setChannel se il canale è corretto (un numero digitato da tastiera) allora sul file stampa il canale;
  * se invece si inserisce una stringa come canale si entra nel catch e sul file stampa il tipo di errore
 
- *provando ad usare la classe logChannel solo per "3.Stampa canale su file" non funziona*/
+ *provando ad usare il meotodo logChannel solo per "3.Stampa canale su file" non funziona*/
 
 package it.alessiacal.tv;
 
